8178f1ab6de0f29751775ba6edcd83c5fbb071da
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-10-07 23:08:29 +0530

    Time: 3 ms (32.59%), Space: 12.1 MB (9.5%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+       int n = s.size();
+      string result;
+
+      for( char c : s){
+        result.push_back(c);
+        if(result.size() >= part.size() &&
+         result.substr(result.size()- part.size()) == part){
+            result.erase(result.size() - part.size());
+         }
+      }
+
+      return result;
+      }
+};
