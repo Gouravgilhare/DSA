ca98318fe2f653484ffb441025552f0b52e418ea
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-10-09 22:09:25 +0530

    Time: 392 ms (65.65%), Space: 46.4 MB (25.76%) - LeetHub

---
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+        vector<int>users = skill;
+        vector<int>products = mana;
+
+        int m = products.size();
+        int n = users.size();
+    
+        vector<long>times(n,0);
+
+        for(int i = 0 ; i < m ; i++)
+        {
+            for(int j = 0 ; j < n; j++){
+                long t = products[i]*users[j];
+
+                if(j==0){
+                    times[0]= times[0]+t;
+                }else{
+                    times[j] = max(times[j], times[j-1])+ t;
+                }
+            }
+            for(int j = n-2; j>=0;j-- ){
+                times[j] = times[j+1] - products[i]*users[j+1];
+            }
+        }
+        return times[n-1];
+    }
+};
