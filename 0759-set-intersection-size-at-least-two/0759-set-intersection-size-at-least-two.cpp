7e385f58b64a1eb0f2e4a5f7a2ca49a5b80c5ae9
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-20 13:58:28 +0530

    Time: 10 ms (39.51%), Space: 23.1 MB (14.4%) - LeetHub

--- 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.cpp
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-
-    static bool check(vector<int>a, vector<int>b){
-        if(a[1]==b[1]){
-            return a[0]>b[0];
-        }
-        return a[1]<b[1];
-    }
-
     int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(), intervals.end(), check);
-        int count = 2;
-        vector<int>maxi(2,0);
-        maxi[0]=intervals[0][1]-1;
-        maxi[1]=intervals[0][1];
 
-        for(int i = 0; i<n ;i++){
-            if(maxi[1]<intervals[i][0]){
-                count += 2;
-                maxi[0]=intervals[i][1]-1;
-                maxi[1]=intervals[i][1];
+        sort(intervals.begin(), intervals.end(), [](auto & a , auto& b) {
+            return a[1] < b[1] || (a[1]==b[1] && a[0]>b[0]);
+        });
 
-            }else if(maxi[0]<intervals[i][0]){
-                count++;
-                maxi[0]=maxi[1];
-                maxi[1]=intervals[i][1]; 
+        int ans = 0 , p1=-1, p2=-1;
+        for(auto x : intervals) {
+            if(x[0] <= p1) continue;
+
+            if(x[0] > p2) {
+                ans+=2;
+                p2 = x[1];
+                p1 = p2-1;
             }
+            else {
+                ans+=1;
+                p1 = p2;
+                p2 = x[1];
+            }
+            // cout<<"p1: " << p1 <<", p2: " << p2 <<" ans: " << ans << endl;
         }
-        return count;
+
+        return ans;
+        
     }
 };
