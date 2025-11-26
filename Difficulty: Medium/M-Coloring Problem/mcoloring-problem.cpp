3c25bf9c0bf0a66b87b923d1cf421b51d2217410
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-27 00:00:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/M-Coloring Problem/mcoloring-problem.cpp
+++ Difficulty: Medium/M-Coloring Problem/mcoloring-problem.cpp
@@ -1,33 +1,35 @@
 class Solution {
   public:
-  
-    bool magic(int node,int v, int m , vector<int>&color, vector<vector<int>>&adj){
+   bool isSafe(int node, int col, vector<int>& color, vector<vector<int>>& adj) {
+        for (int neighbor : adj[node]) {
+            if (color[neighbor] == col) 
+                return false;
+        }
+        return true;
+    }
+    bool magic(int node, int v, int m, vector<int>&color, vector<vector<int>>&adj){
+        
+        
         if(node == v) return true;
         
-        for(int c = 1 ; c<= m ; c++){
-            bool isSafe = true;
+        for(int i = 1 ; i<= m; i++){
+         
             
-            vector<int>temp = adj[node];
-            
-            for(int i = 0 ; i< temp.size(); i++){
-                if(c == color[temp[i]]){
-                    isSafe = false;
-                    break;
-                }
+            if(isSafe(node, i, color,adj)){
+                color[node] = i ;
+                
+                if(magic(node+1, v, m, color, adj))
+                    return true;
+                color[node]=0;
             }
             
-            if(isSafe) {
-                color[node] = c;
-                if(magic(node+1, v,m,color,adj))return true;
-                color[node] = 0;
-            }
         }
         return false;
+        
+        
     }
-  
     bool graphColoring(int v, vector<vector<int>> &edges, int m) {
         // code here
-        
         vector<vector<int>>adj(v);
         
         for(int i = 0 ; i< edges.size(); i++){
@@ -39,6 +41,7 @@ class Solution {
         
         vector<int>color(v,0);
         
-        return magic(0,v,m,color,adj);
+        return magic(0, v,  m, color, adj);
+        
     }
 };
