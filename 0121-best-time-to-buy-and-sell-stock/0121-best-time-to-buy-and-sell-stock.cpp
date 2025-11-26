d5878200337b3f9586a65dd8c419ec0b0ce17a46
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-26 19:12:26 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,13 +5,8 @@ public:
         int max_profit = 0;
 
         for(int i = 1; i < prices.size(); i++){
-            int curr_profit = prices[i] - buy;
-            if(curr_profit > max_profit){
-                max_profit = curr_profit;
-            }
-            if(prices[i] < buy ){
-                buy = prices[i];
-            }
+           max_profit = max(max_profit, prices[i]-buy);
+           buy = min(buy, prices[i]);
         }
 
         return max_profit;
