fe4edc8992d9b5eb3c80bfa5da60a3537872bb31
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-26 20:20:54 +0530

    Time: 3 ms (81.03%), Space: 11.8 MB (14.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(t.size() > s.size()) return "";
-
-        vector<int>mp(256,0);
-        int minLen = INT_MAX;
-        int sIndex = -1;
-        int m = t.size(), n =s.size();
-        int cnt = t.size();
-        int l = 0, r= 0;
-
-        for(auto k : t){
-            mp[k]++;
-        }
-
-            while(r< n){
-                if(mp[s[r]]>0){
-                    cnt--;
-                
-                }
-                    mp[s[r]]--;
-                while(cnt==0){
-
-                    if(r-l+1< minLen){
-                        minLen = r-l+1;
-                        sIndex = l;
-                    }
-                    mp[s[l]]++;
-                    if(mp[s[l]]>0)cnt++;
-                    l++;
-                }
+        int n = s.size();
+        int m = t.size();
+        int minLen = INT_MAX, start= -1;
+        int mp[256] = {0};
+        int  i = 0,l= 0, r= 0, cnt = m;
+       
+            while(i<m){
+                mp[t[i]]++;
+                i++;
+            }
+        
+            while(r<n){
+                if(mp[s[r]] > 0) cnt--;
+                mp[s[r]]--;
                 r++;
-    }
-        return minLen==INT_MAX?"" : s.substr(sIndex,minLen);
+                while(cnt == 0)
+                {
+                    if(r-l< minLen){
+                        minLen = r-l;
+                        start = l;
+                    }
+                        mp[s[l]]++;
+                        if(mp[s[l]]>0) cnt++;
+                        l++;
+                }
+                // r++;
+            }
+        
+
+        return (start == -1? "" : s.substr(start, minLen));
     }
 };
