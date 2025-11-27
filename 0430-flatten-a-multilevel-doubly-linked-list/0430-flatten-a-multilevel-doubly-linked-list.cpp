49a7be59c8ca0495730e8384c8444ddf8f7b9282
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-27 19:37:21 +0530

    Time: 0 ms (100%), Space: 11.6 MB (72.25%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -11,46 +11,49 @@ public:
 
 class Solution {
 public:
-    Node* flattenDFS(Node* head) {
-        Node* last = head;
+
+    Node* flattenDFS(Node*head){
         Node* curr = head;
+        Node* last = head;
 
         while(curr){
-            Node *next= curr->next;
-
+            Node * next = curr->next;
+            
             if(curr->child){
                 Node* child = curr->child;
                 Node* tail = flattenDFS(child);
+                // child->prev =curr;
+                curr->next= child;
+                child->prev= curr;
+                curr->child= nullptr;
 
-                curr->next = child;
-                child->prev = curr;
 
-                curr->child = nullptr;
-
-                if(next) {
-                    tail->next= next;
-                    next->prev= tail;
+                if(next){
+                    tail->next = next;
+                    next->prev = tail;
                 }
 
-
+                // if(!curr->next){
+                //     curr->next = next;
+                //     next->prev= curr;
+                // }
                 last= tail;
-                curr = next;
-
+                curr= next;
             }else{
                 last= curr;
-                curr= next;
+                curr = next;
             }
+            // curr= curr->next;
+            
+
         }
 
         return last;
-
     }
     Node* flatten(Node* head) {
+        if(!head) return NULL;
+        flattenDFS(head);
         
-        if(!head) return nullptr;
-
-       flattenDFS(head);
         return head;
-
-    }
+    }   
 };
