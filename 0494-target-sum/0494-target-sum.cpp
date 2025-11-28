d6773ffded26ef141c5afbaa28a3b446b7ebb4c4
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-28 23:19:26 +0530

    Time: 1 ms (90.88%), Space: 12.4 MB (68.72%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -15,7 +15,27 @@ public:
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         
-        return helper(nums, target, 0,0,0);
+        // return helper(nums, target, 0,0,0);
+        int n = nums.size();
+        int totalSum =0 ;
+        for(int i : nums){
+            totalSum += i;
+        }
+
+        if((totalSum+target)%2!=0 || (totalSum < abs(target)))return 0;
+
+        int P = (totalSum+target)/2;
+        vector<int>dp(P+1, 0);
+        dp[0]=1;
+        for(auto num : nums){
+
+            for(int i = P; i>=num; i-- ){
+                dp[i] += dp[i-num];
+            }
+
+        }
+
+        return dp[P];
 
     }
 };
