058c3a75a0b6632110a8ee3bb8c0e5d873ea7b87
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-28 18:46:02 +0530

    Time: 0 ms (100%), Space: 13.1 MB (11.17%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,13 +2,25 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        sort(citations.begin(), citations.end());
-        int i = 1 ; 
-        while(i<=n){
-            if(citations[n-i] < i)
-                break;
-            i++;
+        // sort(citations.begin(), citations.end());
+        vector<int>bucket(n+1, 0);
+
+        for(int k : citations){
+            if(k>=n)
+            bucket[n]++;
+            else
+            bucket[k]++;
         }
-        return i-1;
+        
+        int total = 0;
+        int i ;
+        for( i = n ; i>=0 ; i--){
+
+            total+=bucket[i];
+
+            if(total >= i)break;
+        }
+
+        return i;
     }
 };
