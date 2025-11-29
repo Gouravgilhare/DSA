cbecb452eae7613db4204b556d670bffc7eb17fd
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-29 21:43:52 +0530

    Time: 127 ms (11.43%), Space: 184.6 MB (20.38%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,43 +1,82 @@
+
 class LRUCache {
-private:
-    int cap;
-    list<pair<int,int>>cache;
-    unordered_map<int,list<pair<int,int>>::iterator>map;
-
-
 public:
+class Node{
+    public:
+    int val , key;
+    Node* next, *prev;
 
+    Node(int key , int val){
+        this->key = key;
+        this->val = val;
 
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+    Node *head ;
+    Node *tail ;
+    
+    unordered_map<int,Node*>mp;
+    int cap= 0;
     LRUCache(int capacity) {
         cap = capacity;
+        head= new Node(-1,-1);
+        tail= new Node(-1,-1);
+        head->next= tail;
+        tail->prev = head;
     }
     
     int get(int key) {
-        if(map.find(key)==map.end()) return -1;
+        if(mp.find(key)==mp.end())return -1;
 
-        auto it = map[key];
-        int value = it->second;
+        Node*ansNode = mp[key];
+        int val = ansNode->val;
+        deleteNode(ansNode);
+        
+        // mp.erase(key);
+        addNode(ansNode);
 
-        cache.erase(it);
-        cache.push_front({key, value});
-        map[key]=cache.begin();
-
-        return value;
+        return val;
     }
+    void addNode(Node* newNode){
+        newNode->prev = head;
+        head->next->prev = newNode;
+        newNode->next = head->next;
+        head->next = newNode;
+    }
+    void deleteNode(Node* oldNode){
+
+        oldNode->prev->next = oldNode->next;
+        oldNode->next->prev = oldNode->prev;
+        oldNode->next= nullptr;
+        oldNode->prev= nullptr;
+        // delete(oldNode);
+
+    }
+
+    
     
     void put(int key, int value) {
-        if(map.find(key)!=map.end()){
-            cache.erase(map[key]);
+        if(mp.find(key)!= mp.end()){
+            Node* oldNode = mp[key];
+            deleteNode(oldNode);
+            mp.erase(key);
+            delete oldNode;
         }
 
-        else if(cache.size()==cap){
-            auto last= cache.back();
-            map.erase(last.first);
-            cache.pop_back();
+        if(mp.size() == cap){
+            Node* node = tail->prev;
+            mp.erase(node->key);
+            // mp.erase(node->key);
+            deleteNode(node);
+            delete node;
         }
 
-        cache.push_front({key,value});
-        map[key]=cache.begin();
+
+        Node* newNode  = new Node(key,value);
+        addNode(newNode);
+        mp[key] = newNode;        
     }
 };
 
