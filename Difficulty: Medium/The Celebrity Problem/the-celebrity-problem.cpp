b4dd424f3fc0db0a6d75f9943331d83619e498fe
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-30 12:22:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -2,32 +2,38 @@ class Solution {
   public:
     int celebrity(vector<vector<int>>& mat) {
         // code here
-        stack<int>st;
         int n = mat.size();
-        for(int i = 0 ; i<n ; i++ ){
+        
+        stack<int>st;
+        for(int i = 0 ; i< n ; i++){
             st.push(i);
         }
         
-        while(st.size() > 1)
-        {
-            int i = st.top();
-                st.pop();
+        while(st.size()>1){
+            int i = st.top() ; 
+            st.pop();
             int j = st.top();
-                st.pop();
+            st.pop();
             
-            if(mat[i][j]== 0){
+            if(mat[i][j]==0){
                 st.push(i);
             }else
                 st.push(j);
+                
+        }
+        int cele  = st.top();
+        
+        for(int i = 0 ; i< n ;i++){
+            // for(int j = 0 ; j < n ;j++){
+            
+            if((i!=cele)&&(mat[cele][i]== 1)|| mat[i][cele] == 0 )return -1;
+             
+                // if() return -1;
+            
         }
         
-        
-        int cele = st.top();
-        
-        for(int i = 0; i< n ; i++){
-            if( (i!= cele) && (mat[i][cele]==0 || mat[cele][i]==1))
-                return -1;
-        }   
         return cele;
+        
+        
     }
 };
