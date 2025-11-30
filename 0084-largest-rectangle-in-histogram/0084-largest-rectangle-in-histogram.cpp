9c59a64eec9a861aa81bee5ef6954f21ff47a90b
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-11-30 20:55:27 +0530

    Time: 24 ms (50.91%), Space: 85.8 MB (33.32%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int maxArea= 0,n =heights.size();
+    int largestRectangleArea(vector<int>& ht) {
+        int n = ht.size();
+        
+        vector<int>right(n);
         stack<int>st;
+        for(int i= n-1 ;i>=0; i-- ){
 
-        for(int i = 0 ; i<= n ; i++){
-            int h =  (i == n ? 0 :  heights[i]);
-
-
-            while(!st.empty() && h < heights[st.top()] ){
-                int height = heights[st.top()];
+            while(!st.empty() && ht[st.top()] >= ht[i]  )
                 st.pop();
-           
-                int right =  i;
-                int left (st.empty()? -1 : st.top());
-                int width = right -left -1; 
 
-
-                maxArea = max(maxArea , height * width);
-            }
+            if(st.empty())right[i]=n;
+            else  right[i]=st.top();
 
             st.push(i);
         }
 
+
+        while(!st.empty()) st.pop();
+
+        vector<int>left(n);
+        // vector<int>st;
+        for(int i= 0 ;i<n; i++ ){
+
+            while(!st.empty() && ht[st.top()] >= ht[i]  )
+                st.pop();
+
+            if(st.empty())left[i]=-1;
+            else  left[i]=st.top();
+
+            st.push(i);
+        }
+
+
+    int maxArea =INT_MIN;
+
+        for(int i = 0 ; i< n ; i++){
+            int area = 0;
+
+            int width= right[i] -left[i]  -1;
+
+            area = width * ht[i];
+
+            maxArea = max(area, maxArea);
+
+
+        }
+
+
+
         return maxArea;
+
+
     }
 };
