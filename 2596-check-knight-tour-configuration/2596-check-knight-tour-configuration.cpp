7d0af94e8fb912ff8f324192df5da148ac97dd05
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-12-01 10:30:21 +0530

    Time: 0 ms (100%), Space: 31.6 MB (99.15%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    bool helper(vector<vector<int>>&grid, int row, int col , int index, vector<pair<int, int>>&pos){
-        
+
+    bool  helper(vector<vector<int>>& grid, int row, int  col,int  cnt ){
         int n = grid.size();
+        if(cnt == n*n-1  )return true;
+        if(grid[0][0]!=0)return false;
+        if(cnt!= grid[row][col]) return false;
+
+        int dx[8]={2 ,2,-2,-2,1,-1,1,-1};
+        int dy[8]={1,-1,1,-1,2,2,-2,-2};
         
-        if(row < 0 || row>= n || col < 0 || col >= n||index != grid[row][col] )
-            return false;
-        if(index == (n*n -1)) return true;
-        
-
-        for(int k = 0 ; k< n*n-1; k++){
-            auto [r1,c1]  = pos[k];
-            auto [r2,c2]  = pos[k+1];
-
-        int dx= abs(r1-r2);
-        int dy= abs(c2-c1);
-
-        if(!((dx == 2 && dy == 1) || (dx==1 && dy==2)))return false;
-
+        int flag= false;
+        for(int i= 0 ;i<8;i++){
+            
+                
+                if(row+dx[i] >=0 && row+dx[i] <n && col+dy[i]>=0  && col+dy[i]<n && grid[row+dx[i]][col+dy[i]] == cnt+1)
+                flag =helper(grid, row+dx[i],col+dy[i], cnt+1);
+                if(flag)return true;
+            
         }
+
+
         
-        return true;
         
+        return false;   
+        
+
+
+
+
     }
-
     bool checkValidGrid(vector<vector<int>>& grid) {
-        if(grid[0][0]!= 0) return false;
-        
-        int n = grid.size();
-        vector<pair<int, int>>pos(n*n);
-        for(int i = 0 ; i< n; i++){
-            for(int j = 0 ; j< n ; j++){
-                pos[grid[i][j]] = {i,j};
-            }
-        }
-
-        
-        return helper(grid, 0,0,0, pos);
+        return helper(grid, 0 ,0,0 );
     }
 };
