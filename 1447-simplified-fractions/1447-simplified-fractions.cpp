8e4bfcd541db8d1aa11cd8c4c41608d350416c5c
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-12-02 18:25:43 +0530

    Time: 19 ms (91.59%), Space: 26.2 MB (35.07%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.cpp
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<string> simplifiedFractions(int n) {
-        vector<string>ans;
-        string s ="";
-        // unordered_set<double>st;
-        for(int i = 1; i<n; i++){
 
-            for(int j = i+1; j<=n ; j++){
-                
-                if(__gcd(i,j)==1){
-                    ans.push_back({to_string(i)+'/'+to_string(j)});
-                }
+     vector<string>ans;
+     for(int i = 1; i<n;i++){
+        for(int j = i+1; j<=n; j++){
+
+            if(__gcd(i,j)==1){
+                ans.push_back(to_string(i)+'/'+to_string(j));
             }
         }
+     }
 
-        return ans;
+     return ans;
+
     }
 };
