4dde2c5464886dbc6777659f16f3f517908e548d
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-12-02 18:13:47 +0530

    Time: 23 ms (6.17%), Space: 44.7 MB (95.56%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = g.size(), m = s.size();
-
-        int l = 0 ; int r =0;
-
+        
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
+
+        int l= 0, r= 0;
+
+        int n = g.size(), m = s.size();
+
         int cnt = 0;
-        
-        while(l< n && r<m){
-            if(g[l] <= s[r]){
+
+        while(l<n && r<m){
+
+            if(g[l]<=s[r]){
                 cnt++;
                 l++;
                 r++;
-            }else {
+            }else{
                 r++;
             }
-        }
+        } 
+
 
         return cnt;
-
 
     }
 };
