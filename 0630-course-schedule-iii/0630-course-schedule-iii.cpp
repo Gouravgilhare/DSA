88535b90583043b49d8d87a868841d0d3fc1ed02
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-12-03 21:02:44 +0530

    Time: 52 ms (35.85%), Space: 67.6 MB (33.29%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int n =courses.size();
+
+        sort(courses.begin(),courses.end(), [](const auto&a, const auto &b){
+            return a[1] < b[1];
+        });
+
+        priority_queue<int>maxHeap;
+        int curr = 0;
+        for(auto c : courses){
+            int duration = c[0];
+            int lastDay= c[1];
+
+            curr += duration;
+            maxHeap.push(duration);
+
+            if(curr > lastDay)
+            {
+                curr -= maxHeap.top();
+                maxHeap.pop();
+            }
+
+        }
+
+
+        return maxHeap.size();
+
+        while(!maxHeap.empty()){
+
+        }
+
+
+    }
+};
