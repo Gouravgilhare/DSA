6a5ae1ecbf895f5209a55b5c5f38cf580d7fb2ea
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2025-12-25 10:19:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -19,23 +19,35 @@ class Solution {
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
         vvi adj(V);
-        vi vis(V);
+        vi vis(V), indegree(V);
         vi topo;
         stack<int>st;
         for(auto &e : edges){
             int u = e[0];
             int v= e[1];
             adj[u].push_back(v);
+            indegree[v]++;
         }
+        
+        queue<int>q;
         for(int i= 0 ; i< V; i++){
-            if(!vis[i]){
-                dfs(i,adj,vis,st);
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
         
-        while(!st.empty()){
-            topo.push_back(st.top());
-            st.pop();
+        
+        
+        while(!q.empty()){
+            int node = q.front();        
+            q.pop();
+            topo.push_back(node);
+            for(auto &a : adj[node]){
+                indegree[a]--;
+                if(indegree[a]==0){
+                    q.push(a);
+                }
+            }
         }
         
         return topo;
