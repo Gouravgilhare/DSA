57bbf6c85149941a9f1fae459ce7b25e33ee0884
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-03-01 23:03:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -3,33 +3,31 @@ class Solution {
     vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
         // Code here
         int n = arr.size();
+        int m = arr[0].size();
         
-        
-        sort (arr.begin(), arr.end());
-        
+        sort(arr.begin(), arr.end());
         vector<vector<int>>ans;
-            int left = arr[0][0];      
-            int right = arr[0][1];      
-        for(int i = 1; i< n ; i++){
-           
-           
-          if(right >= arr[i][0]){
-               right = max(right, arr[i][1]);
-          }
-           
-          else{
-               ans.push_back({left, right});
-               left = arr[i][0];
-               right =arr[i][1];
-          }
-           
-           
-           
-        }
-            ans.push_back({left,right});
+        int first= arr[0][0];
+        int last= arr[0][1];
         
+       for(int i = 1 ; i <n  ; i++){
+           if(last >= arr[i][0] )
+           {
+               if(arr[i][1]  >  last)
+                last =arr[i][1];
+           }
+           else if( last < arr[i][0]){
+               ans.push_back({first, last});
+                first  = arr[i][0];
+                last = arr[i][1];
+           }
+               
+       }
+       
+       ans.push_back({first,last});
+       
+       return ans;
+            
         
-        
-        return ans;
     }
 };
