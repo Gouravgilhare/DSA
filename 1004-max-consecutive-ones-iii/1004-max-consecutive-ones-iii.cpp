96893aa544b3ac69e566ce40bbb7ad32def4c371
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-06-22 22:47:53 +0530

    Time: 0 ms (100%), Space: 69.5 MB (92.76%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,14 +5,17 @@ public:
         int maxLen = 0;
         int zeros = 0;
         int l = 0 , r= 0 ;
-
+        int len = 0;
         while(r< n ){
             if(nums[r]==0) zeros++;
-            while(zeros > k){
+            if(zeros > k){
                 if(nums[l]==0)zeros--;
                 l++;
             }
-            maxLen = max(maxLen, r-l+1);
+            if(zeros <= k )
+            len = r-l+1;
+
+            maxLen = max(maxLen, len);
             r++;
         }
 
