a8a1eb12e8be8de876b876dc7535278e0fbfc274
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-07-25 15:15:41 +0530

    Time: 33 ms (20.19%), Space: 24.4 MB (13.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,47 @@
 class Solution {
 public:
-    void bfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>>& vis) {
-        int n = grid.size();
-        int m = grid[0].size();
+    void bfs(int i, int j, vector<vector<char>>&grid, vector<vector<int>>&vis){
+        int n = grid.size() , m = grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=1;
 
-        queue<pair<int,int>> q;
-        q.push({i, j});
-        vis[i][j] = 1;
+        int r[] = {-1,1,0,0};
+        int c[] = {0,0,-1,1};
 
-        int dx[4] = {1, -1, 0, 0};
-        int dy[4] = {0, 0, 1, -1};
+        while(!q.empty()){
 
-        while (!q.empty()) {
             auto [row, col] = q.front();
             q.pop();
+            vis[row][col] = 1;
 
-            for (int k = 0; k < 4; k++) {
-                int nr = row + dx[k];
-                int nc = col + dy[k];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    !vis[nr][nc] && grid[nr][nc] == '1') {
-
-                    vis[nr][nc] = 1;   // mark HERE
-                    q.push({nr, nc});
+            for(int i = 0 ; i< 4; i++){
+                int l = row+r[i];
+                int k = col + c[i];
+                if(l>-1 && l <n && k > -1 && k < m && !vis[l][k] && grid[l][k] == '1'){
+                    q.push({l,k});
+                    vis[l][k]=1;
                 }
-            }
+            }    
         }
+        
     }
-
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int cnt = 0;
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int island = 0;
+        vector<vector<int>>vis(n, vector<int>(m,0));
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0; j< m; j++){
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
-                    bfs(i, j, grid, vis);
-                    cnt++;
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    island++;
+                    bfs(i,j,grid,vis);
                 }
             }
         }
-        return cnt;
+
+        return island;
     }
 };
