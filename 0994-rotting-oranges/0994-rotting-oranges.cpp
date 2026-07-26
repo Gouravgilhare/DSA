c6d50bbbfff68542057afd781e5e25c3e3a96766
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-07-26 17:40:17 +0530

    Time: 0 ms (100%), Space: 16.8 MB (52.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,51 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
+        int n  = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        // int vis[n][m];
-        int tm = 0,cntF=0;
-        for(int i=0 ; i< n ; i++)
-            for(int j = 0 ; j < m; j++){
+        int fresh= 0;
+        queue<pair<int,int>>q;
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0 ; j< m ; j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},tm});
-                    grid[i][j]=2;
-                }else if(grid[i][j]==1)cntF++;
-            }   
+                    q.push({i,j});
+                }else if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
+        }
 
-
+        if(fresh==0) return 0;
         int dx[] = {1,-1,0,0};
-        int dy []= {0,0,1,-1};
+        int dy[] = {0,0,1,-1};
 
+        int mins = 0;
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t =q.front().second;
-            tm = max(t, tm);
-            q.pop();
-            for(int k = 0 ; k < 4 ; k++){
-                int nr = r + dx[k];
-                int nc = c + dy[k];
-                if(nr>=0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 ) {           
-                    grid[nr][nc]=2;
-                    q.push({{nr,nc},t+1});
-                    cntF--;
+            int size = q.size();
+            bool rotten = false;
+            
+            while(size--){
+
+                auto [row, col] = q.front();
+                q.pop();
+
+
+                for(int i = 0 ; i<4; i++){
+                    int nr = row+dx[i];
+                    int nc = col + dy[i];
+
+                    if(nr>=0 && nr<n  && nc>=0 && nc < m && grid[nr][nc]==1){
+                        q.push({nr,nc});
+                        grid[nr][nc]=2;
+                        fresh--;
+                        rotten= true;
+                    }
                 }
 
             }
-
+                if(rotten) mins++;
         }
-          if(cntF>0)return -1;
-            return tm;
 
+        return fresh == 0? mins: -1;
     }
 };
