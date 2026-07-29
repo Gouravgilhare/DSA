3fa52d000c79499ea6f085e788699ffad8a3f084
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-07-29 09:11:42 +0530

    Time: 63 ms (5.08%), Space: 17.4 MB (97.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,48 +1,67 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
-     int n = numCourses;
-    vector<int>indegree(n,0);
-     vector<vector<int>>adj(n);
+    bool isCycle(int src , vector<bool>&vis, vector<bool>&recPath ,vector<vector<int>>& edges ){
+        vis[src]=true;
+        recPath[src]=true;
 
+        for(int i = 0 ; i< edges.size(); i++){
+            int v = edges[i][0];
+            int u = edges[i][1];
+            
+            if(u == src){
+                if(!vis[v]){
+                    if(isCycle(v,vis,recPath,edges)) return true;
+                }else if( recPath[v]){
+                        return true;
+                }
+            }
+        }
+        recPath[src] = false;
+        return false;
+    }
 
-        for(auto &p: pre){
-            int course = p[0];
-            int pr = p[1];
+    void topoSort(int src, vector<bool>&vis, stack<int>&st, vector<vector<int>>& edges){
+        vis[src] = true;
 
-            adj[pr].push_back(course);
-            indegree[course]++;
-
+        for(int i = 0 ; i< edges.size() ;i++){
+            int v = edges[i][0];
+            int u = edges[i][1];
+            if(u==src)
+            if(!vis[v]){
+                topoSort(v,vis,st,edges);
+            }
         }
 
+        st.push(src);
+    }
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+        vector<bool>vis(n,false);
+        vector<bool>recPath(n,false);
+        vector<int>ans;
 
-        queue<int>q;
         for(int i =0 ; i< n ; i++){
-            if(indegree[i]==0){
-                q.push(i);
+            if(!vis[i]){
+                if(isCycle(i, vis, recPath, edges))
+                    return ans;
             }
         }
 
-        vector<int>order;
+        stack<int>st;
+        vis.assign(n,false);
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            order.push_back(node);
-
-            for(int next : adj[node]){
-
-                indegree[next]--;
-                if(indegree[next]==0)q.push(next);
+        for(int i= 0; i< n ; i++){
+            if(!vis[i]){
+                topoSort(i, vis, st, edges);
             }
         }
 
-        if(order.size()==n){
-            return order;
+
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
         }
 
-        return {};
 
+        return ans;
     }
 };
