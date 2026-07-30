2a9855d012ee339c0d2c86acb1fdfcf8b5df5a48
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-07-30 23:24:43 +0530

    Time: 4 ms (81.22%), Space: 10.4 MB (97.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    int helper(int i , int j , string &a, string&b ,vector<vector<int>>&dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(a[i]==b[j]) return dp[i][j] =  helper(i-1, j-1, a, b, dp);
-        return dp[i][j] = 1 + min( 
-            helper(i-1, j, a, b, dp),
-           min( helper(i, j-1, a, b, dp),
-           helper(i-1, j-1, a, b, dp) ));
-    }
+    // int helper(int i , int j , string &a, string&b ,vector<vector<int>>&dp){
+    //     if(i==0) return j;
+    //     if(j==0) return i;
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     if(a[i-1]==b[j-1]) return dp[i][j] =  helper(i-1, j-1, a, b, dp);
+    //     return dp[i][j] = 1 + min( 
+    //         helper(i-1, j, a, b, dp),
+    //        min( helper(i, j-1, a, b, dp),
+    //        helper(i-1, j-1, a, b, dp) ));
+    // }
     int minDistance(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>>dp(n ,vector<int>(m,-1));
-        return helper(n-1, m-1,s1, s2, dp);
+        if(n==0) return m;
+        // vector<vector<int>>dp(n+1 ,vector<int>(m+1,-1));
+        vector<int>curr(m+1,0), prev(m+1,0);
+        for(int j = 0 ; j<=m ; j++){
+            prev[j]= j;
+        } 
+
+        for(int i = 1 ; i<= n ; i++){
+            curr[0]=i;
+            for(int j = 1; j<=m ; j++){
+
+                if(s1[i-1] == s2[j-1]) curr[j] = prev[j-1];
+                else curr[j] = 1 + min(prev[j], min(curr[j-1], prev[j-1]));
+            }
+            prev=curr;
+        }
+
+        return prev[m];
 
     }
 };
