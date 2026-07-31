9749db62a89f2228027fc66391bbfed7b6d1a5d2
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-07-31 09:27:18 +0530

    Time: 4 ms (45.06%), Space: 18.3 MB (37.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights,
-                          int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        //adj
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it : flights){
 
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto &f : flights) {
-            adj[f[0]].push_back({f[1], f[2]});
+            adj[it[0]].push_back({it[1],it[2]});
         }
 
-        // dist[node][edges_used]
-        vector<vector<int>> dist(n, vector<int>(k + 2, 1e9));
-        dist[src][0] = 0;
+        //pq 
+        queue<pair<int, pair<int,int>>>q;
+        vector<int>dist(n, 1e9 );
+        dist[src]=0;
+        q.push({0,{src,0}}); //distance , node
 
-        queue<pair<int, pair<int,int>>> q;
-        q.push({0, {src, 0}});   // edges_used, {node, cost}
-
-        while (!q.empty()) {
-            auto cur = q.front();
+        while(!q.empty()){
+            int stops = q.front().first;
+            int node = q.front().second.first;
+            int dis = q.front().second.second;
             q.pop();
+            if(stops > k) continue;
+            
+            for(auto &it : adj[node]){
+                int edw = it.second;
+                int adjNode = it.first;
 
-            int edges = cur.first;
-            int node  = cur.second.first;
-            int cost  = cur.second.second;
-
-            if (edges == k + 1) continue;
-
-            for (auto &e : adj[node]) {
-                int nextNode = e.first;
-                int price    = e.second;
-
-                if (cost + price < dist[nextNode][edges + 1]) {
-                    dist[nextNode][edges + 1] = cost + price;
-                    q.push({edges + 1, {nextNode, cost + price}});
+                if(dis + edw < dist[adjNode]){
+                    dist[adjNode] = dis + edw;
+                    q.push({stops +1 ,{adjNode, dist[adjNode]}});
                 }
             }
         }
 
-        int ans = 1e9;
-        for (int i = 0; i <= k + 1; i++) {
-            ans = min(ans, dist[dst][i]);
-        }
-
-        return ans == 1e9 ? -1 : ans;
+        return dist[dst] == 1e9 ? -1 : dist[dst];
     }
 };
