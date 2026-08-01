fe40e9350f1caeb7af0279da0f88e035e8ddb9fa
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-08-01 21:37:25 +0530

    Time: 14 ms (8.01%), Space: 29.6 MB (27.45%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,45 +1,85 @@
 class Solution {
 public:
-    int bfs(int i , int j , vector<vector<int>>&grid){
-        int n  = grid.size();
-        int m  = grid[0].size();
+    vector<int>par, rank,size;
 
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        grid[i][j] = 0;
-        int area = 1;
+    int find(int x){
+        if(par[x] == x) return x;
+        return par[x] = find(par[x]);
+    }
+
+    bool unionByRank(int a, int b){
+        int parA = find(a);
+        int parB = find(b);
+
+        if(parA == parB) return false;
+
+        if(rank[parA] == rank[parB] ){
+            par[parB] = parA;
+            rank[parA]++; 
+            size[parA] += size[parB];
+        }
+        else if (rank[parA] > rank[parB]){
+            par[parB] = parA;
+            size[parA] += size[parB];
+        }else {
+            par[parA] = parB;
+            size[parB] += size[parA];
+        }
+        return true;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+
+        par.resize(n*m); 
+        rank.resize(n*m,0);
+        size.resize(n*m, 1);
+        for(int i = 0 ; i< n*m ; i++){
+            par[i] = i;
+        } 
 
         int dx[] = {1,-1,0,0};
         int dy[] = {0,0,1,-1};
-        while(!q.empty()){
-            auto [row, col] = q.front();
-            q.pop();
-
-            for(int i = 0 ; i< 4 ; i++){
-                int nr = row + dx[i];
-                int nc = col + dy[i];
-
-                if(nr>-1 && nr <n && nc > -1 && nc < m  && grid[nr][nc]==1){
-                    q.push({nr,nc});
-                    grid[nr][nc]=0;
-                    area++;
-                }
-            }
+        int isLands = 0;
+        for(auto g : grid){
+            for(auto b : g)
+                if(b == 1)  isLands++;
         }
-        return area;
-    }
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int cnt= 0;
-        for(int i = 0 ; i<n ;i++){
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0 ; j< m; j++){
+
+                if(grid[i][j]==0) continue;
+                
+                int node = i*m + j;
+
+                for(int k = 0 ; k < 4; k++ ){
+                    int nr = i + dx[k];
+                    int nc = j + dy[k];
+
+                    if(nr <0 || nr>= n || nc < 0 || nc >= m) continue;
+
+                    if(grid[nr][nc]==0) continue;
+
+                    int adjNode = nr*m + nc;
+
+                    if(unionByRank(node, adjNode)){
+                        isLands--;
+                    }
+
+                }
+
+            }
+
+        }
+        int maxArea =0;
+        for(int i = 0 ; i< n ; i++){
             for(int j = 0 ; j< m ; j++){
-                if(grid[i][j]==1)
-                {
-                    cnt = max(cnt,bfs(i,j,grid));
+                if(grid[i][j]==1){
+                    int root = i * m + j;
+                    maxArea = max(maxArea, size[root]);
                 }
             }
         }
-        return cnt;
+        return maxArea;
     }
 };
