d89d37d23f92ef76499f909e0c810ff87a83605a
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-08-02 11:29:45 +0530

    Time: 15 ms (71.78%), Space: 23.6 MB (80.4%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,43 +2,36 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-
-        if (grid[0][0] || grid[n - 1][n - 1])
-            return -1;
-
-        queue<pair<int,int>> q;
+        queue<pair<int,int>>q;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
         q.push({0,0});
-        grid[0][0] = 1;          // mark visited
+        grid[0][0]=1;
 
-        int dx[] = {-1,-1,-1,0,0,1,1,1};
-        int dy[] = {-1,0,1,-1,1,-1,0,1};
-
-        int dist = 1;
-
-        while (!q.empty()) {
+        int dx[] = {0,1,-1,0,1,-1,1,-1};
+        int dy[] = {1,1,1,-1,-1,-1,0,0};
+        int path=1;
+        while(!q.empty()){
             int sz = q.size();
 
-            while (sz--) {
-                auto [x,y] = q.front();
+            while(sz--){
+                auto [r,c] = q.front();
                 q.pop();
+                if (r == n-1 && c == n-1)
+                    return path;
+                for(int k = 0 ; k< 8 ; k++){
+                    int nr = r + dx[k];
+                    int nc = c + dy[k];
 
-                if (x == n-1 && y == n-1)
-                    return dist;
-
-                for (int k = 0; k < 8; k++) {
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n &&
-                        grid[nx][ny] == 0) {
-
-                        grid[nx][ny] = 1;
-                        q.push({nx, ny});
+                    if(nr>=0 && nr< n && nc>=0 && nc< n && grid[nr][nc] == 0 ){
+                        q.push({nr, nc});
+                        grid[nr][nc]=1;
                     }
+
                 }
             }
 
-            dist++;
+            path++;
+
         }
 
         return -1;
