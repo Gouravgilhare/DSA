333d94ad519ef834046f40c9beed70e0c9895dee
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-08-02 12:29:01 +0530

    Time: 64 ms (15.76%), Space: 58 MB (5.05%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,68 +1,51 @@
 class Solution {
 public:
-    unordered_map<string, string>par;
-    unordered_map<string,int>rank;
-
-    string find(string x ){
-        if(par[x] == x) return x;
-
-        return par[x] = find(par[x]);
-    }
-
-    void unionByRank(string a, string b){
-        string parA = find(a);
-        string parB = find(b);
-
-        if(parA == parB) return ;
-
-        if(rank[parA]==rank[parB]) {
-            par[parB]= parA;
-            rank[parA]++;
-        }else if( rank[parA] > rank[parB]){
-            par[parB] = parA;
-        }else{
-            par[parA] = parB;
-        }
-    }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = accounts.size();
+
         unordered_map<string, string>owner;
+        unordered_map<string ,vector<string>> adj   ;
 
-
-        for (auto a : accounts){
-            for(int i =1 ; i < a.size(); i++)
-            {
-                par[a[i]] =  a[i]; // child parent
-                rank[a[i]] = 0;
-                owner[a[i]] = a[0];
+        for(auto &ac : accounts){
+            for(int i = 1 ; i< ac.size(); i++){
+                owner[ac[i]] = ac[0];
+            }
+            for(int i= 2; i<ac.size(); i++){
+                adj[ac[1]].push_back(ac[i]);
+                adj[ac[i]].push_back(ac[1]);
             }
         }
-
-        for(auto a : accounts)
-        {   for(int i = 2; i< a.size() ; i++){
-                unionByRank(a[1], a[i]);
-            }
-        }
-        unordered_map<string, vector<string>>mp;
+        unordered_set<string>vis;
         vector<vector<string>>ans;
+        
+        for(auto &it : owner){
+            
+            string email = it.first;
 
-        for(auto &it : par){
-            string root = find(it.first);
-            mp[root].push_back(it.first);
-        }
+            if(vis.count(email)){
+                continue;
+            }
 
+            queue<string>q;
+            q.push(email);
+            vis.insert(email);
 
-        for (auto &it : mp) {
-            auto &emails = it.second;
-            sort(emails.begin(), emails.end());
+            vector<string>temp;
 
-            vector<string> temp;
-            temp.push_back(owner[it.first]);   // account name
+            while(!q.empty()){
+                string email2 = q.front();
+                q.pop();
+                temp.push_back(email2);
 
-            for (auto &email : emails)
-                temp.push_back(email);
-
-            ans.push_back(temp);
+                for(auto &c : adj[email2] ){
+                    if(!vis.count(c)){
+                        vis.insert(c);
+                        q.push(c);
+                    }
+                }
+            }
+            sort(temp.begin(), temp.end());
+            temp.insert(temp.begin(), owner[email]);
+            ans.push_back(temp);    
         }
 
         return ans;
