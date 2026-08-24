51abf6b15990dd3c526791ea5d10550ebd117c2e
Gourav Gilhare <160500916+Gouravgilhare@users.noreply.github.com>
2026-08-24 21:22:22 +0530

    Time: 12 ms (4.52%), Space: 82.4 MB (14.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
+    int helper(int i , vector<int>&nums,vector<int>&dp){
+        if(i >= nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+
+
+        int take = nums[i] +  helper(i+1, nums,dp);
+        int notTake = nums[i];
+
+        return dp[i] = max(take, notTake);
+    }   
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int maxSum = INT_MIN;
-        int curSum = 0;
+        vector<int>dp(n+1, -1);
+        int ans = INT_MIN;
 
-        //kadanes algorithm
-        for(auto a: nums){
-            curSum += a;
-            maxSum = max(maxSum , curSum);
-            if(curSum <= 0) curSum = 0;
+        for(int i =0 ; i< n ; i++){
+            ans = max(ans, helper(i , nums, dp));
         }
-        return maxSum;
+       return ans;
+
     }
 };
